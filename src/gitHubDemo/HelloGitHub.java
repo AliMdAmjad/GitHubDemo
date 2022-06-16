af76384d56b8b85c5919d705fc6d6c7be918edaf
AliMdAmjad <amjad.ali.sohel@gmail.com>
2022-06-16 00:01:44 -0400

    works

---
+++ src/gitHubDemo/HelloGitHub.java
@@ -0,0 +1,9 @@
+package gitHubDemo;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("I know how to push github to my own created  respiratory ");
+	}
+
+} // works
